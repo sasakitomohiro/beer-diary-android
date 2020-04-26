34eee3e7bd0c723a023c02c86d528c31081f9df9
sasakitomohiro <t0m0.piii.12.07sk@gmail.com>
2020-04-26 21:01:04 +0900

    first commit

---
+++ app/src/main/java/monster/sasakisan/beer_diary_android/MainActivity.kt
@@ -0,0 +1,12 @@
+package monster.sasakisan.beer_diary_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
